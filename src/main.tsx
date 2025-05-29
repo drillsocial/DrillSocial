eca264ac0a167318981a1e3f2177f03e39cfa462
Matt Hansen <matthansen708@icloud.com>
2025-05-29 13:19:23 -0500

    main.tsx lime green

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('app');
-  if (root) {
-    ReactDOM.createRoot(root).render(
-      <React.StrictMode>
-        <h1 style={{ color: 'limegreen' }}>🔥 IT WORKS!</h1>
-      </React.StrictMode>
-    );
-  } else {
-    console.error('Could not find #app element');
-  }
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1 style={{ color: 'limegreen' }}>🔥 IT WORKS! 🔥</h1>
+  </React.StrictMode>
+);
