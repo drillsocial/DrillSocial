47ecc39afe7f6c9e650fe6e5ba8f0bbe9e8a4167
Matt Hansen <matthansen708@icloud.com>
2025-05-29 13:14:22 -0500

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const rootElement = document.getElementById('app');
-  if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
+  const root = document.getElementById('app');
+  if (root) {
+    ReactDOM.createRoot(root).render(
       <React.StrictMode>
         <h1 style={{ color: 'limegreen' }}>🔥 IT WORKS!</h1>
       </React.StrictMode>
     );
   } else {
-    console.error("Could not find #app element");
+    console.error('Could not find #app element');
   }
 });
