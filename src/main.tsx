cbd24b9b88a3cb6a40ff79e2b4bba505a7b10b56
Matt Hansen <matthansen708@icloud.com>
2025-05-29 13:11:20 -0500

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('app')!).render(
-  <React.StrictMode>
-    <h1 style={{ color: 'limegreen' }}>🔥 IT WORKS!</h1>
-  </React.StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('app');
+  if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+      <React.StrictMode>
+        <h1 style={{ color: 'limegreen' }}>🔥 IT WORKS!</h1>
+      </React.StrictMode>
+    );
+  } else {
+    console.error("Could not find #app element");
+  }
+});
