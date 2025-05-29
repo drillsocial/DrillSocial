c7f78eb9c91fdae555e7fb6a99fe1b04a0893fd9
Matt Hansen <matthansen708@icloud.com>
2025-05-29 11:59:01 -0500

    app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-// src/App.tsx
-export default function App() {
+function App() {
   return (
-    <div style={{ textAlign: 'center', color: 'hotpink', marginTop: '2rem' }}>
-      <h1>🚀 DrillSocial is Running</h1>
-      <p>You should be seeing this message.</p>
+    <div>
+      <h1>Hello, DrillSocial is working!</h1>
     </div>
   );
 }
+
+export default App;
