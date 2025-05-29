96a93972e7137dd73c38749465d864a255971948
Matt Hansen <matthansen708@icloud.com>
2025-05-29 16:23:11 -0500

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found. Ensure there is a <div id="root"> in index.html.');
+}
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
